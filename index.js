dea3e42de27e6acfc37fbefa2c411b1fd20b4c0b
Pedro Henrique F. Dias <pedroarapua@gmail.com>
2022-08-25 16:42:27 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const tracer = require("./tracing")("nodejs-opentelemetry");
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const port = process.env.PORT || 3000;
+const mongodbHost = process.env.MONGODB_HOST || 'localhost';
+const mongodbPort = process.env.MONGODB_PORT || 27017;
+const mongodbUrl = `mongodb://${mongodbHost}:${mongodbPort}`;
+let db;
+
+const app = express();
+app.use(express.json());
+
+app.get("/todo", async (_, res) => {
+  const todos = await db.collection("todos").find({}).toArray();
+  res.send(todos);
+});
+
+app.get("/todo/:id", async (req, res) => {
+  const todo = await db.collection("todos").findOne({ id: req.params.id });
+  res.send(todo);
+});
+
+const startServer = () => {
+  MongoClient.connect(mongodbUrl, (err, client) => {
+    db = client.db("todo");
+    db.collection("todos").insertMany([
+     { id: "1", title: "Buy groceries" },
+     { id: "2", title: "Install Aspecto" },
+     { id: "3", title: "buy my own name domain" },
+   ]);
+  });
+
+  app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+  });
+};
+startServer();
